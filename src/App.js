16381a47f3b025671a2c89ed859dbc0b404c15d1
ny.yi <2n.nayeon.yi@daum.net>
2022-04-05 13:22:38 +0900

    try to catch the error

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import React, { useReducer, useMemo } from 'react';
-
-import UserList from './UserList';
-import CreateUser from './CreateUser';
-
-import reducer from './reducer';
-import { initialState } from './store/initialState';
-
-
-function countActiveUsers(users) {
-  console.log('활성 사용자 수를 세는중...');
-  return users.filter(user => user.active).length;
-};
-
-export const UserDispatch = React.createContext(null);
+import React from 'react';
+import User from './User';
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const { users } = state;
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
-
+  const user = {
+    id: 1,
+    username: 'velopert'
+  };
   return (
-    <UserDispatch.Provider value={dispatch}>
-      <CreateUser
-        // username={username}
-        // email={email}
-        // onChange={onChange}
-        // onCreate={onCreate}
-      />
-      <UserList
-        users={users}
-        // onToggle={onToggle}
-        // onRemove={onRemove}
-      />
-      <div>활성사용자 수 : {count}</div>
-    </UserDispatch.Provider>
+    <ErrorBoundary>
+      <User />
+    </ErrorBoundary>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css';
+import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: 'https://0905f3be6c9e447b9a43f18ab613d7d7@o1189419.ingest.sentry.io/6309807',
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
