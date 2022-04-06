d4bed29053bbd87ba8b3936fff4b369ffa1f1e2c
ny.yi <2n.nayeon.yi@daum.net>
2022-04-06 10:37:17 +0900

    get data with useReducer

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,32 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
 
+function reducer(state, action) {
+  switch(action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null
+  });
 
   const fetchUsers = async () => {
+    dispatch({ type: 'LOADING' });
     try {
-      // reset state of error and users
-      setError(null);
-      setUsers(null);
-      // make loading state true
-      setLoading(true);
-      // get response
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users'
       );
-      setUsers(response.data);
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: 'ERROR', error: e });
     }
-    setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>loading..</div>;
   if (error) return <div>{error.toString()}</div>;
