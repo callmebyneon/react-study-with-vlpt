828b1f6ba63efbc5e706f23adb4bd6eebc2e32b4
ny.yi <2n.nayeon.yi@daum.net>
2022-04-08 13:03:41 +0900

    apply codes to fix reloading

--- src/modules/posts.jsx
+++ src/modules/posts.jsx
@@ -1,5 +1,11 @@
 import * as postsAPI from '../api/posts';
-import { createPromiseThunk, reducerUtils, handleAsyncActions } from '../lib/asyncUtils';
+import {
+  createPromiseThunk,
+  reducerUtils,
+  handleAsyncActions,
+  createPromiseThunkById,
+  handleAsyncActionsById
+} from '../lib/asyncUtils';
 
 
 
@@ -16,25 +22,8 @@ const GET_POST_ERROR = 'GET_POST_ERROR';
 
 //************* thunk functions
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-// export const getPosts = () => async dispatch => {
-//   dispatch({ type: GET_POSTS });
-//   try {
-//     const posts = await postsAPI.getPosts();
-//     dispatch({ type: GET_POSTS_SUCCESS, posts });
-//   } catch (e) {
-//     dispatch({ type: GET_POSTS_ERROR, error: e });
-//   }
-// };
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
-// export const getPost = id => async dispatch => {
-//   dispatch({ type: GET_POST });
-//   try {
-//     const post = await postsAPI.getPostById(id);
-//     dispatch({ type: GET_POST_SUCCESS, post });
-//   } catch (e) {
-//     dispatch({ type: GET_POST_ERROR, error: e });
-//   }
-// };
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+
 
 
 
@@ -43,18 +32,6 @@ const initialState = {
   posts: reducerUtils.initial(),
   post: reducerUtils.initial()
 };
-// const initialState = {
-//   posts: {
-//     loading: false,
-//     data: null,
-//     error: null
-//   },
-//   post: {
-//     loading: false,
-//     data: null,
-//     error: null
-//   }
-// };
 
 
 
@@ -63,50 +40,14 @@ export default function posts(state = initialState, action) {
     case GET_POSTS:
     case GET_POSTS_SUCCESS:
     case GET_POSTS_ERROR:
-      const postsReducer = handleAsyncActions(GET_POSTS, 'posts');
+      const postsReducer = handleAsyncActions(GET_POSTS, 'posts', true);
       return postsReducer(state, action);
     case GET_POST:
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
-      const postReducer = handleAsyncActions(GET_POST, 'post');
+      const postReducer = handleAsyncActionsById(GET_POST, 'post');
       return postReducer(state, action);
     default:
       return state;
   }
 }
-// export default function posts(state = initialState, action) {
-//   switch (action.type) {
-//     case GET_POSTS:
-//       return {
-//         ...state,
-//         posts: reducerUtils.loading()
-//       }
-//     case GET_POSTS_SUCCESS:
-//       return {
-//         ...state,
-//         posts: reducerUtils.success(action.payload)
-//       }
-//     case GET_POSTS_ERROR:
-//       return {
-//         ...state,
-//         posts: reducerUtils.error(action.error)
-//       }
-//     case GET_POST:
-//       return {
-//         ...state,
-//         post: reducerUtils.loading()
-//       }
-//     case GET_POST_SUCCESS:
-//       return {
-//         ...state,
-//         post: reducerUtils.success(action.payload)
-//       }
-//     case GET_POST_ERROR:
-//       return {
-//         ...state,
-//         post: reducerUtils.error(action.error)
-//       }
-//     default:
-//       return state
-//   }
-// }
