ceb0354fc88aa44aa312cb4199a392f797241c49
ny.yi <2n.nayeon.yi@daum.net>
2022-04-08 10:21:14 +0900

    Create branch and use middleware(logger, thunk)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './module';
+import rootReducer from './modules';
+import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
 
 import { composeWithDevTools } from '@redux-devtools/extension';
-/** (redux-devtools-extension) Package moved to @redux-devtools/extension.
- * Chrome extension: https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
- * npm (@redux-devtools/extension): https://www.npmjs.com/package/@redux-devtools/extension
- */
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
